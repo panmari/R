db16d016ff3c16340cc7e17f0c591f1c54c801e5
panmari <smmuzi@gmail.com>
2012-08-09 16:32:07 +0200

    prettified Multinomial CBs.R

--- MultinomialCBs.R
+++ MultinomialCBs.R
@@ -6,12 +6,15 @@ MultinomialCBs <- function(TX, p0, overall.alpha=0.05)
 	
 	a <- rep(0,levels)  # lower bounds
 	b <- rep(1,levels)  # upper bounds
+	inIntervall <- rep(TRUE, levels) # initialize with true
+	
 	for (j in 1:levels)
 	{
 		result <- binom.test(TX[j],n,
 			conf.level=1-overall.alpha/levels)
 		a[j] <- result$conf.int[1]
 		b[j] <- result$conf.int[2]
+		inIntervall[j] <- (TX[j] >= a[j] && TX[j] <= b[j])
 	}
-	return(cbind(TX,p.emp,a,b, p0))
+	return(cbind(Practical.values=TX,p.emp,p.theo=p0, a,b, sig=inIntervall))
 }
